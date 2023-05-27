8dd1946ef9b7e54e14b11f3424f8f6c0012ece03
Diego Moura <diego.moura@theideaofnow.com>
2023-05-26 21:47:00 -0300

    add new hello endpoint

--- api/main.go
+++ api/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"qrcode-login/controllers"
 	"qrcode-login/database"
 	"qrcode-login/middlewares"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	database.Connect("root:root@tcp(localhost:3306)/qrcode?parseTime=true")
+	database.Connect("root:root@tcp(db:3306)/qrcode?parseTime=true")
 	database.Migrate()
 
 	router := initRouter()
@@ -19,6 +20,7 @@ func initRouter() *gin.Engine {
 	router := gin.Default()
 	api := router.Group("/api")
 	{
+		api.GET("/hello", controllers.Index)
 		api.POST("/token", controllers.GenerateToken)
 		api.POST("/user/register", controllers.RegisterUser)
 		secured := api.Group("/secured").Use(middlewares.Auth())
