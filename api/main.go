e59c099f35a76a025d174b1fc521f5a7a9a32282
Diego Moura <diego.moura@theideaofnow.com>
2023-06-05 20:44:39 -0300

    add cors

--- api/main.go
+++ api/main.go
@@ -13,6 +13,8 @@ func main() {
 	database.Migrate()
 
 	router := initRouter()
+
+	router.Use(CORS())
 	router.Run(":8080")
 }
 
@@ -21,13 +23,32 @@ func initRouter() *gin.Engine {
 	api := router.Group("/api")
 	{
 		api.GET("/hello", controllers.Index)
-		api.POST("/token", controllers.GenerateToken)
+		api.POST("/auth/login", controllers.GenerateToken)
 		api.POST("/user/register", controllers.RegisterUser)
-		secured := api.Group("/secured").Use(middlewares.Auth())
+
+		secured := api.Group("/").Use(middlewares.Auth())
 		{
 			secured.GET("/ping", controllers.Ping)
+			secured.GET("/auth/user", controllers.UserInfo)
 		}
 	}
 
 	return router
+}
+
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
